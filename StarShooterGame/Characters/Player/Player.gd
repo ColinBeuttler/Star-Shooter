bb287e3fed4b30b240db09aac8b06fbae109bb38
colin <beutt102@mail.chapman.edu>
2024-07-15 14:18:30 -0700

    firstGameUpdate

---
+++ StarShooterGame/Characters/Player/Player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+export (int) var speed = 300
+
+var input_vector = Vector2.ZERO
+
+func _physics_process(delta):
+	
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	global_position.x += input_vector.x * speed * delta
+	global_position.y += input_vector.y * speed * delta
+	
+	global_position.x = clamp(global_position.x, 40, 500)
+	global_position.y = clamp(global_position.y, 30, 930)
+	
